1c3075c38bc19c79008894a13903d05e7b5ac5ea
Alex Kotusenko <alexkotusenko@gmail.com>
2025-08-23 23:52:47 -0400

    scaffolding more

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,56 @@ pub enum Version {
 }
 
 impl Version {
-    pub fn try_into(s: &str) -> Option<Self> {
+    pub fn try_from(s: &str) -> Option<Self> {
         todo!();
     }
 }
 
 
 /// A header 
-struct Header{} // TODO
+// TODO
+pub struct Header {
+    key: String,
+    value: String,
+}
+
+impl Header {
+    pub fn try_from(s: &str) -> Option<Self> {
+        todo!();
+    }
+}
+
+type StatusCodeInt = usize;
+
+/// The status code returned with responses
+pub enum StatusCode {
+    Ok, // or what is it called?
+    // Etc
+}
+
+impl StatusCode {
+    pub fn as_int(&self) -> StatusCodeInt { 
+        todo!();
+    }
+
+    pub fn try_from_int(&self) -> Option<Self> {
+        todo!();
+    }
+}
+
+pub struct Body(String);
+
+pub enum Method {
+    Get,
+    Post, 
+    // TODO rest
+}
+
+pub struct RequestPacket {
+    method: Method,
+    url: String,
+    // TODO rest
+} 
+
+// TODO
+pub struct ResponsePacket {}
